eddae74de37291ea9697a7db2218e17df38d4687
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2022-12-15 13:26:53 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 # run myapp
-# notice: already "COPY app.json /etc/myapp/app.json" at Dockerfile
-/usr/bin/myapp -config=/etc/myapp/app.json &
+# notice: already "COPY app.json /etc/EasyBot/app.json" at Dockerfile
+/usr/bin/EasyBot -config=/etc/EasyBot/app.json &
 
 #run nginx
 # CMD ["nginx", "-g", "daemon off;"]
 nginx -g 'daemon off;'
 # /usr/local/nginx/sbin/nginx -g 'daemon off;'
-
-
 
